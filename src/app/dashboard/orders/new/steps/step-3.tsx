a3f9b0eea7cc10b73fa8dcf6e857db0a41c372ee
reda. <redadahmani34@gmail.com>
2026-01-12 11:10:15 +0100

    feat: StepThree I done the first step of it.

--- src/app/dashboard/orders/new/steps/step-3.tsx
+++ src/app/dashboard/orders/new/steps/step-3.tsx
@@ -34,58 +34,52 @@ export default function StepOne({ onNext, onBack, currentStep = 3 }: Props) {
       {/* Form Title */}
       <div className="pb-2">
         <h2 className="text-2xl font-bold text-gray-900">
-          Informations de la commande
+          Produits de la commande
         </h2>
         <p className="text-sm text-gray-600 mt-1">
-          Sélectionnez le préfixe, le canal et le mode de livraison
+          Ajoutez les produits avec leurs options
         </p>
       </div>
       {/* Form Fields */}
-      <div className="flex flex-col space-y-6">
+      <div className="flex flex-col space-y-6  ">
         {/* Ligne 1 */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div className="flex justify-center items-center space-y-7 gap-6 ">
           <Field>
             <FieldLabel htmlFor="select-prefix">
-              Préfixe De Commande *
+              Sélectionner un produit *
             </FieldLabel>
             <Select>
-              <SelectTrigger className="w-full pl-4 pr-10 h-12">
+              <SelectTrigger className="w-full pl-4 h-12">
                 <SelectValue
                   id="select-prefix"
-                  placeholder="Sélectionner Un Préfixe"
+                  placeholder="Sélectionner un produit à ajouter"
                 />
               </SelectTrigger>
               <SelectContent>
-                <SelectItem value="bog">BOG - Atelier Bogny</SelectItem>
-                <SelectItem value="eric">ERIC - Eric</SelectItem>
-                <SelectItem value="web">WEB - Site Web</SelectItem>
+                <SelectItem value="coeur">Coeur - 55,00 €</SelectItem>
+                <SelectItem value="pra">
+                  PRA (Prisma Allongé) - 85,00 €
+                </SelectItem>
+                <SelectItem value="prc">
+                  PRC (Prisma Carrée) - 65,00 €
+                </SelectItem>
+                <SelectItem value="rectangle_grand">
+                  Rectangle grand - 75,00 €
+                </SelectItem>
+                <SelectItem value="rectangle_petit">
+                  Rectangle petit - 45,00 €
+                </SelectItem>
               </SelectContent>
             </Select>
           </Field>
-
-          <Field>
-            <FieldLabel htmlFor="select-canal">Canal De Réception *</FieldLabel>
-            <Select>
-              <SelectTrigger className="w-full pl-4 pr-10 h-12">
-                <SelectValue id="select-canal" placeholder="Atelier" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="atelier">Atelier</SelectItem>
-                <SelectItem value="email">Email</SelectItem>
-                <SelectItem value="shopify">Shopify</SelectItem>
-                <SelectItem value="web">Site Web</SelectItem>
-                <SelectItem value="phone">Téléphone</SelectItem>
-                <SelectItem value="whatsapp">WhatsApp</SelectItem>
-              </SelectContent>
-            </Select>
-          </Field>
+          <Button>+</Button>
         </div>
 
         {/* Ligne 2 */}
         <Field>
           <FieldLabel>Mode De Livraison *</FieldLabel>
           <Select>
-            <SelectTrigger className="w-full pl-4 pr-10 h-12">
+            <SelectTrigger className="w-full pl-4  h-12">
               <SelectValue placeholder="Retrait Atelier" />
             </SelectTrigger>
             <SelectContent>
